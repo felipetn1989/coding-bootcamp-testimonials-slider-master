f70920c2a27065c02b57f75baec87e3b0ad12565
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-19 12:12:53 -0300

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,27 +1,17 @@
-nextButton.addEventListener("click", nextPage);
+nextButton.addEventListener("click", changePage);
+previousButton.addEventListener("click", changePage);
 
-function nextPage() {
-  tanyaPage.classList.remove("flex");
-  tanyaPage.classList.add("hidden");
-  johnPage.classList.remove("hidden");
-  johnPage.classList.add("flex");
-  nextButton.src = "../images/icon-next-unav.svg";
-  nextButton.classList.remove("hover:cursor-pointer");
-  previousButton.src = "../images/icon-prev.svg";
-  previousButton.classList.add("hover:cursor-pointer");
-  previousButton.addEventListener("click", previousPage);
-  nextButton.removeEventListener("click");
-}
-
-function previousPage() {
-  tanyaPage.classList.remove("hidden");
-  tanyaPage.classList.add("flex");
-  johnPage.classList.remove("flex");
-  johnPage.classList.add("hidden");
-  nextButton.src = "../images/icon-next.svg";
-  nextButton.classList.add("hover:cursor-pointer");
-  nextButton.addEventListener("click", nextPage);
-  previousButton.src = "../images/icon-prev-unav.svg";
-  previousButton.classList.remove("hover:cursor-pointer");
-  previousButton.removeEventListener("click");
+function changePage() {
+  tanyaPage.classList.toggle("flex");
+  tanyaPage.classList.toggle("hidden");
+  johnPage.classList.toggle("hidden");
+  johnPage.classList.toggle("flex");
+  nextButton.src = tanyaPage.classList.contains("flex")
+    ? "../images/icon-next.svg"
+    : "../images/icon-next-unav.svg";
+  nextButton.classList.toggle("hover:cursor-pointer");
+  previousButton.src = johnPage.classList.contains("flex")
+    ? "../images/icon-prev.svg"
+    : "../images/icon-prev-unav.svg";
+  previousButton.classList.toggle("hover:cursor-pointer");
 }
