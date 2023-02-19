6c1d06a3cf765e9558e7a7e5e9858a1101b8edb5
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-19 13:39:02 -0300

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,20 @@
+/*As the page is loaded, the nextButton is the available one (since we are on page 1 of the testimonals). Therefore I create a boolean named isNextButtonActive 
+to be equal to true whenever nextButton is active (able to be clicked on). If it is false, whe are on page 2 and the previousButton is the available one */
+
 let isNextButtonActive = true;
 
+//Next we add the event listeners to both buttons, invoking the function changePage whenever they are clicked on
+
+nextButton.addEventListener("click", changePage);
+previousButton.addEventListener("click", changePage);
+
+/* This is the function invoked by the click events. It has one parameter, event, which represents the event that triggered the function (in this case the event is the mouse click on any of the buttons */
+
 function changePage(event) {
-  const clickedButton = event.target;
+  const clickedButton =
+    event.target; /*event.target refers to the target element of the click event (in this code, it is one of the buttons, and this is stored in the clickedButton variable, representing which of the buttons we clicked on) */
+
+  /* Next, the code checks two conditions. If the nextButton is active and we try to click on the previous button (the inactive one in this case), the function will simply return without doing anything. The same happens if previousButton is active (nextButton inactive) and we try to click on nextButton */
 
   if (
     (isNextButtonActive && clickedButton === previousButton) ||
@@ -9,6 +22,8 @@ function changePage(event) {
   ) {
     return;
   }
+
+  /* If we click on the current active button, the function is allowed to proceed and do the appropriate  chages to display the correct page, change the icons color and cursor */
 
   isNextButtonActive = !isNextButtonActive;
   tanyaPage.classList.toggle("flex");
@@ -24,6 +39,3 @@ function changePage(event) {
     : "../images/icon-prev-unav.svg";
   previousButton.classList.toggle("hover:cursor-pointer");
 }
-
-nextButton.addEventListener("click", changePage);
-previousButton.addEventListener("click", changePage);
