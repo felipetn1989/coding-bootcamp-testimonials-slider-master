df2163874bf3dccbc867a0744c41ec986c31b2e4
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-02-20 08:16:56 -0300

    update

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ function changePage(event) {
   johnPage.classList.toggle("hidden");
   johnPage.classList.toggle("flex");
   nextButton.src = tanyaPage.classList.contains("flex")
-    ? "../images/icon-next.svg"
-    : "../images/icon-next-unav.svg";
+    ? "./images/icon-next.svg"
+    : "./images/icon-next-unav.svg";
   nextButton.classList.toggle("hover:cursor-pointer");
   previousButton.src = johnPage.classList.contains("flex")
-    ? "../images/icon-prev.svg"
-    : "../images/icon-prev-unav.svg";
+    ? "./images/icon-prev.svg"
+    : "./images/icon-prev-unav.svg";
   previousButton.classList.toggle("hover:cursor-pointer");
 }
